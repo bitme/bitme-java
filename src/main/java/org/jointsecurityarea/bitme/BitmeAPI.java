fcccdd2679a33c60f0266ab2d7d905c1f5d45036
Hank <hank@crow.local>
2013-10-28 18:53:20 -0400

    Implementing more API and tests

--- src/main/java/org/jointsecurityarea/bitme/BitmeAPI.java
+++ src/main/java/org/jointsecurityarea/bitme/BitmeAPI.java
@@ -2,10 +2,15 @@ package org.jointsecurityarea.bitme;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -16,15 +21,19 @@ import org.bouncycastle.crypto.params.KeyParameter;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * BitmeAPI
  * Author: Erik Gregg
  */
 public class BitmeAPI {
+    Logger logger = Logger.getLogger("BitmeAPI");
     private static final String API_URLROOT = "https://bitme.com/rest";
     private final String apiSecret;
     private final String apiKey;
@@ -34,12 +43,26 @@ public class BitmeAPI {
         this.apiKey = apiKey;
         this.apiSecret = apiSecret;
         this.httpClient = HttpClients.createDefault();
+        this.logger.setLevel(Level.ALL);
     }
-
+    private JSONObject query(String url)
+    { return query(url, false); }
+    private JSONObject query(String url, boolean auth)
+    { return query(url, auth, null); }
     private JSONObject query(String url, boolean auth, List<NameValuePair> params)
+        { return query(url, auth, params, "GET"); }
+    private JSONObject query(String url, boolean auth, List<NameValuePair> params, String method)
     {
         JSONObject object = null;
-        HttpGet get = new HttpGet(BitmeAPI.API_URLROOT + url);
+        HttpUriRequest req;
+        if(method == null || method.equals("GET"))
+        {
+            req = new HttpGet(BitmeAPI.API_URLROOT + url);
+        }
+        else
+        {
+            req = new HttpPost(BitmeAPI.API_URLROOT + url);
+        }
         if(params == null) params = new ArrayList<NameValuePair>();
 
         if(auth)
@@ -50,22 +73,36 @@ public class BitmeAPI {
             byte[] resBuf = new byte[hmac.getMacSize()];
             Date date = new Date();
             long nonce = date.getTime() * 1000 + 1000000;
-            params.add(new BasicNameValuePair("nonce", (new Long(nonce)).toString()));
+            params.add(0, new BasicNameValuePair("nonce", (new Long(nonce)).toString()));
             String to_hash = URLEncodedUtils.format((Iterable<? extends NameValuePair>) params, null);
             hmac.update(to_hash.getBytes(), 0, to_hash.getBytes().length);
             hmac.doFinal(resBuf, 0);
-            get.addHeader("Rest-Key", this.apiKey);
-            get.addHeader("Rest-Sign", new String(Base64.encodeBase64(resBuf)));
-            try {
-                get.setURI(new URI(BitmeAPI.API_URLROOT + url + "?" + to_hash));
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
+            req.addHeader("Rest-Key", this.apiKey);
+            logger.info("Rest-Key: " + this.apiKey);
+            req.addHeader("Rest-Sign", new String(Base64.encodeBase64(resBuf)));
+            logger.info("Rest-Sign: " + new String(Base64.encodeBase64(resBuf)));
+            logger.info("Params: " + to_hash);
+            if(req instanceof HttpGet)
+            {
+                try {
+                    ((HttpGet)req).setURI(new URI(BitmeAPI.API_URLROOT + url + "?" + to_hash));
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            } else
+            {
+                try {
+                    ((HttpPost)req).setEntity(new UrlEncodedFormEntity(params));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
         CloseableHttpResponse response = null;
         try {
-            response = this.httpClient.execute(get);
+            logger.info("Final URL: " + req.getURI().toString());
+            response = this.httpClient.execute(req);
             HttpEntity entity = response.getEntity();
             object = new JSONObject(EntityUtils.toString(entity));
         } catch (IOException e) {
@@ -85,23 +122,26 @@ public class BitmeAPI {
 
     public JSONObject verify_credentials()
     {
-        return query("/verify-credentials", true, null);
+        return query("/verify-credentials", true);
     }
     public JSONObject accounts()
     {
-        return query("/accounts", true, null);
+        return query("/accounts", true);
     }
     public JSONObject bitcoin_address()
     {
-        return query("/bitcoin-address", true, null);
+        return query("/bitcoin-address", true);
     }
-    /* create_order */
     public JSONObject open_orders()
     {
-        return query("/orders/open", true, null);
+        return query("/orders/open", true);
     }
     /* cancel_order */
-    /* get_transaction */
+    public JSONObject get_transaction(String id)
+    {
+        return query("/transaction/" + id, true);
+    }
+
     public JSONObject get_transactions()
         { return get_transactions("BTC"); }
     public JSONObject get_transactions(String currency_cd)
@@ -118,5 +158,33 @@ public class BitmeAPI {
         params.add(new BasicNameValuePair("order_by", order_by));
         params.add(new BasicNameValuePair("page", (new Integer(page)).toString()));
         return query("/transactions/" + currency_cd, true, params);
+    }
+
+    public JSONObject create_order(String order_type_cd,
+                                   int quantity,
+                                   String rate)
+        { return create_order(order_type_cd, quantity, rate, "LTCBTC"); }
+    public JSONObject create_order(String order_type_cd,
+                                   int quantity,
+                                   String rate,
+                                   String currency_pair)
+    {
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("order_type_cd", order_type_cd));
+        params.add(new BasicNameValuePair("quantity", (new Integer(quantity)).toString()));
+        params.add(new BasicNameValuePair("rate", rate));
+        params.add(new BasicNameValuePair("currency_pair", currency_pair));
+        return query("/order/create", true, params, "POST");
+    }
+
+    public JSONObject cancel_order(String uuid)
+    {
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("uuid", uuid));
+        return query("/order/cancel", true, params, "POST");
+    }
+    public JSONObject get_order(String uuid)
+    {
+        return query("/order/" + uuid, true);
     }
 }
--- src/test/java/org/jointsecurityarea/bitme/BitmeAPITest.java
+++ src/test/java/org/jointsecurityarea/bitme/BitmeAPITest.java
@@ -1,15 +1,22 @@
 package org.jointsecurityarea.bitme;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.junit.Assert;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class BitmeAPITest {
+    Logger logger = Logger.getLogger("BitmeAPITest");
     private BitmeAPI api;
     @Before
     public void setUp() throws Exception {
         this.api = new BitmeAPI(BitmeAPIKeys.API_KEY, BitmeAPIKeys.API_SECRET);
+        logger.setLevel(Level.ALL);
     }
 
     @Test
@@ -42,5 +49,33 @@ public class BitmeAPITest {
         JSONObject object = this.api.get_transactions();
         // Validate
         Assert.assertTrue(object.has("transactions"));
+    }
+    @Test
+    public void testGetTransaction() throws Exception {
+        JSONObject object = this.api.get_transactions();
+        Assert.assertTrue(object.has("transactions"));
+        JSONArray txs = object.getJSONArray("transactions");
+        JSONObject member = txs.getJSONObject(0);
+        JSONObject tx = this.api.get_transaction(member.getString("id"));
+        // Validate
+        Assert.assertTrue(tx.has("transaction"));
+    }
+
+    /* NOTE: You need at least 0.001 BTC available in your account for this to work */
+    @Test
+    public void testCreateQueryCancelOrders() throws Exception {
+        JSONObject object = this.api.create_order("BID", 1, "0.001");
+        logger.info("Create Order returned:" + object.toString());
+        Assert.assertTrue(object.has("order"));
+        String order_uuid = object.getJSONObject("order").getString("uuid");
+
+        // Get
+        object = this.api.get_order(order_uuid);
+        Assert.assertTrue(object.has("order"));
+
+        // Cancel
+        object = this.api.cancel_order(order_uuid);
+        logger.info("Cancel Order returned:" + object.toString());
+        Assert.assertTrue(object.has("order"));
     }
 }
