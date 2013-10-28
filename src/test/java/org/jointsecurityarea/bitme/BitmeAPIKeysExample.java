1f25b10c1fd21cd95062d80b024a0f3ee8f030bb
Hank <hank@crow.local>
2013-10-28 02:17:19 -0400

    Initial gh-pages commit

--- src/test/java/org/jointsecurityarea/bitme/BitmeAPIKeysExample.java
+++
@@ -1,7 +0,0 @@
-package org.jointsecurityarea.bitme;
-
-public class BitmeAPIKeysExample {
-    // COPY THIS FILE TO BitmeAPIKeys.java to enable testing with your keys
-    static final String API_KEY = "YOUR KEY HERE";
-    static final String API_SECRET = "YOUR SECRET HERE";
-}
